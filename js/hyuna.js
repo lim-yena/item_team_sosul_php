863e3a548b143f61f54dd359637ceeac5f79f353
hyuna37 <h.hyunn37@gmail.com>
2025-02-24 18:17:21 +0900

    메인상단퀵메뉴 및 js오류수정

--- js/hyuna.js
+++ js/hyuna.js
@@ -1,3 +1,6 @@
-document.querySelector(".download_coupon").addEventListener("click", function() {
-    alert("쿠폰이 다운로드되었습니다!");
-  });
+window.addEventListener("DOMContentLoaded", function(){
+  document.querySelector(".download_coupon").addEventListener("click", function() {
+      alert("쿠폰이 다운로드되었습니다!");
+    });
+
+})
--- main/main_center_icon.php
+++ main/main_center_icon.php
@@ -1,14 +1,35 @@
 <ul class="h_main_quickmenu">
     <li>
-        <a href=""><img src="" alt="바로가기">퀵메뉴1</a>
+        <a href=""><img src="//item-team-sosul.vercel.app/img/icon/quickicon_animal.svg" alt="반려동물 바로가기"><span>반려동물</span></a>
     </li>
     <li>
-        <a href=""><img src="" alt="바로가기">퀵메뉴2</a>
+        <a href=""><img src="//item-team-sosul.vercel.app/img/icon/quickicon_caravan.svg" alt="카라반 바로가기"><span>카라반</span></a>
     </li>
     <li>
-        <a href=""><img src="" alt="바로가기">퀵메뉴3</a>
+        <a href=""><img src="//item-team-sosul.vercel.app/img/icon/quickicon_beach.svg" alt="해수욕장 바로가기"><span>해수욕장 근처</span></a>
     </li>
     <li>
-        <a href=""><img src="" alt="바로가기">퀵메뉴4</a>
+        <a href=""><img src="//item-team-sosul.vercel.app/img/icon/quickicon_mountain.svg" alt="마운틴 뷰 바로가기"><span>마운틴 뷰</span></a>
+    </li>
+    <li>
+        <a href=""><img src="//item-team-sosul.vercel.app/img/icon/quickicon_monthcoupon.svg" alt="이달의 쿠폰 바로가기"><span>이달의 쿠폰</span></a>
+    </li>
+    <li>
+        <a href=""><img src="//item-team-sosul.vercel.app/img/icon/quickicon_child.svg" alt="아이와 함께 바로가기"><span>아이와 함께</span></a>
+    </li>
+    <li>
+        <a href=""><img src="//item-team-sosul.vercel.app/img/icon/quickicon_new.svg" alt="신규오픈바로가기"><span>신규오픈</span></a>
+    </li>
+    <li>
+        <a href=""><img src="//item-team-sosul.vercel.app/img/icon/quickicon_only.svg" alt="오직여기바로가기"><span>오직여기</span></a>
+    </li>
+    <li>
+        <a href=""><img src="//item-team-sosul.vercel.app/img/icon/quickicon_night.svg" alt="야경바로가기"><span>야경</span></a>
+    </li>
+    <li>
+        <a href=""><img src="//item-team-sosul.vercel.app/img/icon/quickicon_pool.svg" alt="수영장바로가기"><span>수영장</span></a>
+    </li>
+    <li>
+        <a href=""><img src="//item-team-sosul.vercel.app/img/icon/quickicon_ocean.svg" alt="바다근처바로가기"><span>바다근처</span></a>
     </li>
 </ul>
