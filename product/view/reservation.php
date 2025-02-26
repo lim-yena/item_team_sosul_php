5b18aab0a5d13c00d18f5907592342012e1ce85d
qkrthdud <chzfkchzfk@gmail.com>
2025-02-26 15:32:21 +0900

    예약확인

---
+++ product/view/reservation.php
@@ -0,0 +1,26 @@
+상세페이지 예약안내 들어갈 페이지입니다.
+<!-- 
+    각페이지마다 공통으로 들어갈 css가 있습니다. 
+    뷰페이지에 들어갈 공통 class를 확인 후에 작업해주세요 
+ -->
+ <div class="reservation_sy">
+    <h4 class="list_tit">예약안내</h4>
+        <div class="res_sy_textbox basic_txt">
+            <div class="res_sy_text">
+                <h5>예약가능</h5>
+                <p>2025.03.01 까지</p>
+            </div>
+            <div class="res_sy_text">
+                <h5>최대예약</h5>
+                <p>최대 3박 / 2개 까지</p>
+            </div>
+            <div class="res_sy_text">
+                <h5>오픈주기</h5>
+                <p>매월 3일 10시 오픈 (1개월 단위)</p>
+            </div>
+            <div class="res_sy_text">
+                <h5>다음예약</h5>
+                <p>2025.03.03 10시 오픈 2025.04.30까지</p>
+            </div>
+        </div>
+</div>
