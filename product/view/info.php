3f12bf584cc17e9a819afd1eb9eaca9cc2d1a497
hyuna37 <h.hyunn37@gmail.com>
2025-02-26 18:08:16 +0900

    상세페이지메인 코딩

--- product/view/info.php
+++ product/view/info.php
@@ -1,4 +1,4 @@
-상세페이지 시설소개 들어갈 페이지입니다.
+<!-- 상세페이지 시설소개 들어갈 페이지입니다. -->
 <!-- 
     각페이지마다 공통으로 들어갈 css가 있습니다. 
     뷰페이지에 들어갈 공통 class를 확인 후에 작업해주세요 
@@ -7,9 +7,11 @@
     <div>
        <div class="h_vinfo_tit">
            <h4 class="list_tit">여주 블루마린캠핑장</h4>
-       
-           <p class="basic_txt">아이들과 신나게 놀 수 있는 캠핑장!!!</p>
-           <p class="basic_txt">가족캠핑하기 좋은 곳이며 넓은 주차공간과 텐트마다 공간이 충분해서 대형텐트로 단체캠핑이 가능합니다.</p>
+
+           <div>
+               <p class="basic_txt">아이들과 신나게 놀 수 있는 캠핑장!!!</p>
+               <p class="basic_txt">가족캠핑하기 좋은 곳이며 넓은 주차공간과 텐트마다 공간이 충분해서 대형텐트로 단체캠핑이 가능합니다.</p>
+           </div>
            <p class="basic_txt">가족과 함께 즐겁고 행복한 캠핑이 될 수 있도록 노력하겠습니다.</p>
        </div>
    </div>
@@ -19,7 +21,7 @@
            <img src="//item-team-sosul.vercel.app/img/image/detail_img_4.jpg" alt="캠핑장시설사진">
        </div>
    
-       <div >
+       <div>
            <h4 class="list_tit">소제목</h4>
    
            <section>
@@ -27,16 +29,20 @@
                    <img src="//item-team-sosul.vercel.app/img/image/detail_img_1.jpg" alt="수영장전경">
                    <img src="//item-team-sosul.vercel.app/img/image/detail_img_3.jpg" alt="수영장전경">
                </div>
-               <h5 class="basic_txt">수영장</h5>
-               <p>수영장이 있어 수영을 할 수 있습니다.</p>
+               <div class="h_vinfo_textbox">
+                   <h5 class="basic_txt">수영장</h5>
+                   <p>수영장이 있어 수영을 할 수 있습니다.</p>
+               </div>
            </section>
            <section>
                <div class="h_vinfo_img">
                    <img src="//item-team-sosul.vercel.app/img/image/detail_img_2.jpg" alt="매점전경">
                    <img src="//item-team-sosul.vercel.app/img/image/detail_img_6.jpg" alt="매점전경">
                </div>
-               <h5 class="basic_txt">매점</h5>
-               <p>매점에서 맛난 음식을 사먹을 수 있습니다.</p>
+               <div class="h_vinfo_textbox">
+                   <h5 class="basic_txt">매점</h5>
+                   <p>매점에서 맛난 음식을 사먹을 수 있습니다.</p>
+               </div>
            </section>
        </div>
    </div>
