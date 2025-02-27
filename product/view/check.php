6d10df51c72fa0268e41de139b172f5540077bc7
qkrthdud <chzfkchzfk@gmail.com>
2025-02-27 13:22:28 +0900

    예약체크 작업

---
+++ product/view/check.php
@@ -0,0 +1,41 @@
+<!-- 
+    각페이지마다 공통으로 들어갈 css가 있습니다. 
+    뷰페이지에 들어갈 공통 class를 확인 후에 작업해주세요 
+ -->
+    <div class=rescheck_sy>
+        <div class=rescheck_sy1>
+            <img src="//item-team-sosul.vercel.app/img/image/image 2.png" alt="">
+            <span class=rc_sy1>카라반 1호</span><br>
+            <span class=rc_sy2>성인 2명, 미성년2인</span>
+        </div>
+        <div class=rescheck_sy2>
+            <div class="roombt_sy2">
+                <div class=saleall_sy>
+                <span class=sale_sy>20%</span>
+                <span class=price_sy>64,000원&nbsp;~</span>
+                </div>
+            </div>
+        </div>
+        <div class=rescheck_sy3>
+            <span class=gn_sy2> 입실 </span><span class=gn_sy3>2025.12.19</span> 
+            <span class=gn_sy2>~&nbsp;&nbsp;&nbsp;&nbsp;퇴실</span> 
+            <span class=gn_sy3>2025.12.21</span> <br/>
+        </div>
+        <div class=rescheck_sy3>
+            <span class=gn_sy2>대인</span> 
+            <span class=gn_sy3>3 명</span> 
+            <span class=gn_sy2>소인</span> 
+            <span class=gn_sy3>2 명</span>
+        </div>
+        <div class=rescheck_sy4>
+            <span>사이트 요금</span> 
+            <span>1명 추가 + 64,000원</span>
+        </div>
+        <div class=rescheck_sy5>
+            <span>총액</span> 
+            <span>70,000원</span>
+        </div>
+        <div class="rescheck_sy6">
+            <button class="sebt_sy">예약하기</button>
+        </div>
+    </div>
